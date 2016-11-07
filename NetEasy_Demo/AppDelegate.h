53fa08e8b4aa5dfa0ebf722a81031e4109b56502
梁志成 <Lzc_file@LzcMac.local>
2016-11-07 14:45:09 +0800

    Initial Commit

---
+++ NetEasy_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetEasy_Demo
+//
+//  Created by 梁志成 on 2016/11/7.
+//  Copyright © 2016年 梁志成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NetEasy_Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NetEasy_Demo
+//
+//  Created by 梁志成 on 2016/11/7.
+//  Copyright © 2016年 梁志成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
